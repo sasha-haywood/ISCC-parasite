d07b7f8901b61e81cffb19f11a513a319754d900
sasha-haywood <56835820+sasha-haywood@users.noreply.github.com>
2021-03-31 19:45:15 -0400

    Update parasite.R
    editing comments

--- parasite.R
+++ parasite.R
@@ -11,6 +11,9 @@ alldata <- read_csv("alldata.csv",
                     "June", "July", "August", "October")),
                     sex = col_factor(levels = c("F", "M"))))
 alldata = alldata[,c(21, 22, 10, 11, 8, 6, 7, 5, 3, 4, 2)]
+
+# Go ahead and remove any observation where we don't have either of the possible
+# response variables.
 alldata = alldata[!is.na(alldata$gill) | !is.na(alldata$acantho),]
 
 
@@ -22,34 +25,48 @@ ggplot(alldata, aes(gill)) +
   geom_histogram()
 ggplot(alldata, aes(acantho)) +
   geom_histogram()
-# I think this indicates that we need a hurdle or zero inflated poisson model.
+# Indicates poisson model.
 
 ggplot(alldata, aes(month, gill)) +
   geom_jitter()
 ggplot(alldata, aes(month, acantho)) +
   geom_jitter()
-# I don't think we have enough months to treat them as anything other than a factor.
-
-# I am not sure we need a mixed model on this.  I don't see what would be the 
-# random effect.  
-# I also don't think we need to include year.  But I could be wrong.
+# Treat months as a factor.
+# Can not treat as time series because we are missing most months.
 
 pairs(alldata[-c(1, 2)])
 pairs(alldata[c(6:7)])
-# obviously, mass and length are highly correlated.  Mass only has 2 NAs and SL has 
-# 44, so it makes sense to keep mass.
-
-z.gill = zeroinfl(gill ~ Gen_freq_all + Gen_freq_species + alleles + mass +
-                    sex + month + species, alldata, dist = "poisson")
-summary(z.gill)
+# Obviously, mass and length are highly correlated so we have to choose 1.  
+# Mass only has 2 missing values and SL has 44, so it makes sense to keep mass.
 
 ggplot(alldata, aes(gill, fill = species)) +
   geom_histogram()
 
-# all the orangethroat species have gill=0.  If we remove those samples and look at 
-# them separately, the rest of  the data does not require zero inflated model. 
-# Plain poisson works.  Otherwise, species determines the binomial part of the model,
-# and then everything except Gen_freq_species is significant in the poisson part.
+##### Poisson model with gill as the response
+
+poiss.gill = glm(gill ~ Gen_freq_all + Gen_freq_species + alleles + 
+                   mass +
+                   sex + month + species, alldata, family = "poisson")
+summary(poiss.gill)
+
+# We can leave all factors in, but if we do model selection, the model 
+# improves if we remove sex. Might be good since so many sex values are NA.
+
+drop1(poiss.gill, test = "Chi")
+
+m1 = glm(gill ~ Gen_freq_all + Gen_freq_species + alleles + 
+           mass + month + species, alldata, family = "poisson")
+
+# More model selection suggests we can remove Gen_freq_all and Gen_freq_species
+step(m1)
+
+# recommended model 
+final.with = glm(gill ~ alleles + mass + month + species, 
+                 alldata, family = "poisson")
+
+
+# all the orangethroat species have gill=0.  We can build a model with or
+# without them and just consider that species separately.
 
 data.without.orange = alldata %>%
   filter(species != "orangethroat")
@@ -59,8 +76,9 @@ poiss.gill.without = glm(gill ~ Gen_freq_all + Gen_freq_species + alleles +
                     sex + month + species, data.without.orange, family = "poisson")
 summary(poiss.gill.without)
 drop1(poiss.gill.without, test = "Chi")
-# sex can be dropped (lower AIC if it is removed) which is good because lots of 
-# missing values for sex
+
+# Sex can be dropped (lower AIC if it is removed) which is good because lots of 
+# missing values for sex.
 
 m1 = glm(gill ~ Gen_freq_all + Gen_freq_species + alleles + 
            mass + month + species, data.without.orange, family = "poisson")
@@ -70,45 +88,29 @@ step(m1)
 final.without = glm(gill ~ alleles + mass + month + species, 
                     data.without.orange, family = "poisson")
 
-# actually, poisson works WITH the orangethroat species as part of it.
-poiss.gill = glm(gill ~ Gen_freq_all + Gen_freq_species + alleles + 
-                   mass +
-                   sex + month + species, alldata, family = "poisson")
-summary(poiss.gill)
-drop1(poiss.gill, test = "Chi")
-# can remove sex
-m1 = glm(gill ~ Gen_freq_all + Gen_freq_species + alleles + 
-           mass + month + species, alldata, family = "poisson")
-step(m1)
-
-# recommended model 
-final.with = glm(gill ~ alleles + mass + month + species, 
-                 alldata, family = "poisson")
-
-
+# Can compare these possible models
 summary(poiss.gill)
 summary(final.with)
 summary(poiss.gill.without)
 summary(final.without)
-# significant: Alleles, mass, month, species.  
+
+# Significant: Alleles, mass, month, species.  
 # Not so significant: Gen_freq_all, Gen_freq_species, sex
-# it's hard to tell about sex because there are so many missing values, but I think 
+# It's hard to tell about sex because there are so many missing values, but I think 
 # it is safe to remove sex from the model.  If we remove the NAs, sex is only
 # significant at .07 level.  Client should decide.  If the only purpose it to show
 # that alleles is significant when controlling for everything else, then she might
 # want to leave everything in. Alleles is more significant when controlling for 
 # everything.
 
-z.acantho = zeroinfl(acantho ~ Gen_freq_all + Gen_freq_species + alleles + mass +
-                    sex + month + species, alldata, dist = "poisson")
-summary(z.acantho)
 
-# It looks like month determines the binomial part (and maybe mass), and nothing 
-# else is significant. All June and August observations are acantho = 0
+
+
+############# Poisson model with acantho as response
 
 ggplot(alldata, aes(acantho, fill = month)) +
   geom_histogram()
-
+# All June and August observations are acantho = 0
 
 # here's what it looks like as a poisson model
 poiss.acantho = glm(acantho ~ Gen_freq_all + Gen_freq_species + alleles + mass +
@@ -118,6 +120,7 @@ summary(poiss.acantho)
 ggplot(alldata, aes(acantho, fill = month)) +
   geom_histogram()
 # looks like only species and mass matters
+
 drop1(poiss.acantho, test = "Chi")
 # can remove sex
 m1 = glm(acantho ~ Gen_freq_all + Gen_freq_species + alleles + 
@@ -125,6 +128,8 @@ m1 = glm(acantho ~ Gen_freq_all + Gen_freq_species + alleles +
 step(m1)
 # preferred model does not include alleles but that is what we are testing for,
 # so leave it in anyway.
+
+# These models show some heteroskedasticity.  We can try 
 acantho.final = glm(acantho ~ alleles + mass + month + species, alldata, 
                     family = "poisson")
 summary(poiss.acantho)
